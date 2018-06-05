8b1ba45f5c91c438094cdb7111c4a2fe227974c9
Hillevi Eklöw <hillevieklow@gmail.com>
2018-06-05 17:38:02 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -30,8 +30,8 @@ def adding
                   "oil paintings" => 3
                  }
 
-shipping_manifest["muskets"] = 2
-shipping_manifest["gun powder"] = 4
+  shipping_manifest["muskets"] = 2
+  shipping_manifest["gun powder"] = 4
 
-shipping_manifest
+  shipping_manifest
 end
